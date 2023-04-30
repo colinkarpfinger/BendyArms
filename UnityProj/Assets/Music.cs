65038140816417c98dc97fb4f12eff321a4c9a73
Mathew A. Kramer <108158617+superluumic@users.noreply.github.com>
2023-04-30 14:22:55 -0400

    Fix BGM transition points

--- UnityProj/Assets/Music.cs
+++ UnityProj/Assets/Music.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Music : MonoBehaviour
 {
+    public Canvas containerCanvas;
+    private Text containerText;
     private FMOD.Studio.EventInstance instance;
     private BeatSystem bS;
     private int containerCount;
@@ -11,6 +14,7 @@ public class Music : MonoBehaviour
     void Start()
     {
         bS = GetComponent<BeatSystem>();
+        containerText = containerCanvas.GetComponent<Text>();
         instance = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Gameplay/BGM_layered");
         instance.start();
         bS.AssignBeatEvent(instance); 
@@ -19,6 +23,7 @@ public class Music : MonoBehaviour
 
     void Update() 
     {
+        containerText.text = "Containers: " + containerCount.ToString();
         if (Input.GetKeyDown(KeyCode.Space))
         {
             containerCount += 1;
