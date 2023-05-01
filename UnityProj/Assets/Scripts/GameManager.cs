c6c99faf56e707e8f216aefa8b86e685d4f0362d
Colin Karpfinger <colin.karpfinger@gmail.com>
2023-05-01 11:23:20 -0700

    new font and images are in

--- UnityProj/Assets/Scripts/GameManager.cs
+++ UnityProj/Assets/Scripts/GameManager.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private float startTime = 60f;
+    [SerializeField] private int startTime = 60;
+    [SerializeField] private int timeBonusPerContainer = 5;
     private int containersDelivered = 0;
-    private float timer;
+    private int timer;
 
     public delegate void ScoreChanged(int containers);
 
     public event ScoreChanged scoreChanged;
+    
+    public delegate void TimerChanged(int timeLeft);
+
+    public event TimerChanged timerChanged;
     void Start()
     {
         containersDelivered = 0;
+        timer = startTime;
+        StartCoroutine(UpdateTime());
     }
 
     public void IncrementContainerCount()
     {
         containersDelivered++;
+        timer += timeBonusPerContainer;
         Debug.Log("Containers Delivered: "+containersDelivered.ToString());
         OnScoreChanged();
+        OnTimeChanged();
     }
 
+    IEnumerator UpdateTime()
+    {
+        while (timer > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            timer--;
+            OnTimeChanged();
+        } 
+        TimerEnded();
+    }
     void OnScoreChanged()
     {
         scoreChanged?.Invoke(containersDelivered);
+    }
+
+    void OnTimeChanged()
+    {
+        timerChanged?.Invoke(timer);
+    }
+
+    void TimerEnded()
+    {
+        //show game over screen or something 
     }
 }
--- UnityProj/Assets/Scripts/UIScoreManager.cs
+++ UnityProj/Assets/Scripts/UIScoreManager.cs
@@ -8,20 +8,27 @@ public class UIScoreManager : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;   //todo might want to move this to a scriptable object or something 
     [SerializeField] private TextMeshProUGUI textContainerScore;   //todo might want to move this to a scriptable object or something 
+    [SerializeField] private TextMeshProUGUI textTime;   
 
     private void OnEnable()
     {
         gameManager.scoreChanged += UpdateScore;
+        gameManager.timerChanged += UpdateTime;
     }
 
     private void OnDisable()
     {
         gameManager.scoreChanged -= UpdateScore;
+        gameManager.timerChanged -= UpdateTime;
     }
 
     private void UpdateScore(int containers)
     {
         textContainerScore.SetText(containers.ToString());
+    }
+    private void UpdateTime(int time)
+    {
+        textTime.SetText(time.ToString());
     }
 
 }
