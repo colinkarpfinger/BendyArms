10d02d90ed2a777e88ea475b21ef31343fca5f04
Mathew A. Kramer <108158617+superluumic@users.noreply.github.com>
2023-05-01 20:13:17 -0400

    Stop audio on reload

--- UnityProj/Assets/Scripts/UIGameOver.cs
+++ UnityProj/Assets/Scripts/UIGameOver.cs
@@ -41,6 +41,8 @@ public class UIGameOver : MonoBehaviour
     public void ButtonPressRestart()
     {
         this.gameObject.SetActive(false);
+        FMOD.Studio.Bus masterBus = FMODUnity.RuntimeManager.GetBus("Bus:/");
+        masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         SceneManager.LoadScene(sceneName);
     }
 
